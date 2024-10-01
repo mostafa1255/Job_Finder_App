a4010ac6427d94629fdfd6131b1945fc23c76c6c
Mostafa <yassermostafa620@gmail.com>
2024-10-01 15:49:40 +0300

    fix UI issues in home screen

--- lib/features/home/view/pages/home_screen.dart
+++ lib/features/home/view/pages/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'recommended_jops_card.dart';
 import 'job_card.dart';
 import 'bottom_navigation.dart';
@@ -7,101 +8,104 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        actions: [
-          CircleAvatar(
-            backgroundImage: AssetImage('assets/profile.jpg'), // Add your profile image here
-            radius: 20,
-          ),
-          SizedBox(width: 16),
-        ],
-      ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.symmetric(vertical: 30.h, horizontal: 15.w),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Welcome Back!', style: TextStyle(fontSize: 24)),
-              Text('John Lucas 👋', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-
-              SizedBox(height: 16),
-
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Featured Jobs', style: TextStyle(fontSize: 20)),
-                  Text('See all', style: TextStyle(fontSize: 16, color: Colors.blue)),
+                  Text('Welcome Back!', style: TextStyle(fontSize: 24)),
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/profile.jpg'),
+                    radius: 20,
+                  ),
                 ],
               ),
-
-              SizedBox(height: 10),
-
-              // Featured job cards
-              SizedBox(
-                height: 200, // Adjust height for job cards
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    JobCard(
-                      company: 'Google',
-                      title: 'Product Designer',
-                      salary: '\$160,000/year',
-                      location: 'California, USA',
-                      tags: ['Design', 'Full-Time', 'Junior'],
-                      color: Colors.blueAccent,
-                    ),
-                    JobCard(
-                      company: 'Facebook',
-                      title: 'Sr Engineer',
-                      salary: '\$120,000/year',
-                      location: 'New York, USA',
-                      tags: ['Engineering', 'Full-Time', 'Senior'],
-                      color: Colors.grey,
-                    ),
-                  ],
+              Text('John Lucas 👋',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+              SizedBox(height: 16),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 12.w, vertical: 15.h),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.search, color: Colors.grey),
+                      SizedBox(width: 10.w),
+                      Expanded(
+                        child: Text(
+                          'Search for jobs...',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-
-              SizedBox(height: 20),
-
-              Row(
+              SizedBox(height: 40.h),
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Recommended Jobs', style: TextStyle(fontSize: 20)),
-                  Text('See all', style: TextStyle(fontSize: 16, color: Colors.blue)),
+                  Text('Featured Jobs',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text('See all',
+                      style: TextStyle(fontSize: 16, color: Colors.grey)),
                 ],
               ),
-
-              SizedBox(height: 10),
-
-              // Recommended job cards
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 20),
+              SizedBox(
+                  height: 200.h,
+                  child: ListView.builder(
+                      itemCount: 3,
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return JobCard(
+                          company: 'Facebook',
+                          title: 'Sr Engineer',
+                          salary: '\$120,000/year',
+                          location: 'New York, USA',
+                          tags: ['Engineering', 'Full-Time', 'Senior'],
+                          color: Colors.blue,
+                        );
+                      })),
+              SizedBox(height: 40.h),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  RecommendedJopsCard(
-                    company: 'Dribbble',
-                    title: 'UX Designer',
-                    salary: '\$80,000/year',
-                    color: Colors.pinkAccent,
-                    companyLogo: Image.asset('assets/images/b.jpeg'),
-                  ),
-                  SizedBox(height: 10),
-                  RecommendedJopsCard(
-                    company: 'Facebook',
-                    title: 'Sr Engineer',
-                    salary: '\$96,000/year',
-                    companyLogo: Image.asset('assets/images/c.jpeg'),
-
-                    color: Colors.blueAccent,
-                  ),
+                  Text('Recommended Jobs',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text('See all',
+                      style: TextStyle(fontSize: 16, color: Colors.grey)),
                 ],
               ),
-
-
+              SizedBox(height: 20.h),
+              SizedBox(
+                height: 200.h,
+                child: ListView.builder(
+                    itemCount: 3,
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return RecommendedJopsCard(
+                        company: 'Dribbble',
+                        title: 'UX Designer',
+                        salary: '\$80,000/year',
+                        color: Colors.pinkAccent,
+                        companyLogo: Image.asset('assets/images/b.jpeg'),
+                      );
+                    }),
+              )
             ],
           ),
         ),
